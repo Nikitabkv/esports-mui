c511481a26b67747b8feddf78009b08509b7dd7c
Niki <email@email.email>
2023-09-07 23:00:54 +0300

    chore: one more fix for github pages

--- src/app/dota/page.tsx
+++ src/app/dota/page.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import {Box} from '@mui/material';
+import {Box, Typography} from '@mui/material';
 import HomeGallery from "@/components/HomeGallery/HomeGallery";
+import HomeNewsList from "@/components/HomeNewsList/HomeNewsList";
 
 const MainBoxSx = {
   display: 'flex',
@@ -11,6 +12,10 @@ const DotaPage = () => {
   return (
     <Box sx={MainBoxSx}>
       <HomeGallery/>
+      <Typography variant='h2' color='#ff3535'>
+        Тестик
+      </Typography>
+      <HomeNewsList />
     </Box>
   );
 }
