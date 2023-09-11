bd0d4c6ab4843eb68bdd024aab8f022a053c7cab
Nikita <nikitabakumenko@yandex.ru>
2023-09-11 17:57:49 +0300

    fix: next.config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
-    output: export,
+    output: 'export',
     modularizeImports: {
         '@mui/icons-material': {
             transform: '@mui/icons-material/{{member}}',
