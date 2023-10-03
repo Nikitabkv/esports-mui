a70af6885cfebf9724cf82a2f0fc9f5a7bd6fcca
Nikita <nikitabakumenko@yandex.ru>
2023-10-03 16:48:34 +0300

    fix: popular posts = popular posts

--- src/app/category/[slug]/page.tsx
+++ src/app/category/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import {getCategories, getPostsByCategory} from "@/lib/api/api"
+import {getCategories, getPostsByCategory, getPopularPosts} from "@/lib/api/api"
 import Box from "@mui/material/Box"
 import Stack from "@mui/material/Stack"
 import AsideCard from "@/components/AsideCard/AsideCard"
@@ -21,13 +21,14 @@ const CategoryPage = async ({ params }: { params: { slug: string } }) => {
   const {slug} = params
   const category = slug === 'Dota 2' ? 'Dota' : slug
   const posts = await getPostsByCategory(category);
+  const popularPosts = await getPopularPosts()
 
   return (
     <Box display={'flex'} flexDirection={'column'}>
       <Stack direction={{medium: "column", default: "row"}} spacing={1}>
         <NewsList posts={posts.slice(0, 20)}/>
         <Box component='aside' sx={{flexBasis: '352px'}}>
-          <AsideCard posts={posts.slice(0, 15)} title={'Поплуярные статьи'}/>
+          <AsideCard posts={popularPosts.slice(0, 12)} title={'Поплуярные статьи'}/>
         </Box>
       </Stack>
     </Box>
